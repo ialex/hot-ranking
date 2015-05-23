3165dfd9d8b2e1b8f4fe79a162dd45e33177a8b8
Alejandro Villanueva <admin@ialex.org>
2015-05-23 14:51:01 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+"use strict";
+
+// Math behind: http://www.seomoz.org/img/upload/reddit_cf_algorithm.png
+// Reference: https://github.com/reddit/reddit/blob/master/r2/r2/lib/db/_sorts.pyx#L27-L57
+
+module.exports = function hot_ranking(ups, downs, date) {
+  var upvotes = ups || 0;
+  var downvotes = downs || 0;
+  var score = upvotes - downvotes;
+  var order, sign, seconds, rank;
+
+  if( toString.call(date) !== "[object Date]" ){
+    throw new Error("You must pass a valid date object");
+  }
+  
+  order = Math.log10( Math.max( Math.abs(score), 1) );
+  sign = score > 0 ? 1 :
+         score < 0 ? -1 :
+         0 ;
+  seconds = (date.getTime() / 1000) - 1134028003;
+  rank = sign * order + seconds / 45000;
+  return rank.toPrecision(11);
+};
---
+++ test.js
@@ -0,0 +1,10 @@
+"use strict";
+
+var expect = require("chai").expect;
+
+
+describe("Ranking algorithm", function() {
+  it("Must throw if date is invalid", function() {
+    expect("").to.be.equal("bar");
+  });
+});
